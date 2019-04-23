922d0ccdbf2a67cbf609fae89a5ab1a633f3e6f1
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2019-04-23 21:15:57 +0300

    Add project files.

---
+++ PatternObserverViaInterfaces/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternObserverViaInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermostat thermostat = new Thermostat();
+
+            thermostat.CurrentTemperature = 60;
+
+            float temperature = thermostat.CurrentTemperature;
+
+            Heater heater = new Heater(temperature);
+
+            Cooler cooler = new Cooler(temperature);
+
+            thermostat.CurrentTemperature = 100;
+
+            heater.Temperature = thermostat.CurrentTemperature;
+
+            cooler.Temperature = thermostat.CurrentTemperature;
+        }
+    }
+
+    public class Cooler
+    {
+        public Cooler(float temperature) => Temperature = temperature;
+
+        public float Temperature { get; set; }
+
+        public void Update(float newTemperature)
+            =>  Console.WriteLine(newTemperature > Temperature
+                ? $"Cooler: On. Changed:{Math.Abs(newTemperature - Temperature)}"
+                : $"Cooler: Off. Changed:{Math.Abs(newTemperature - Temperature)}");
+    }
+
+    public class Heater
+    {
+        public Heater(float temperature) => Temperature = temperature;
+
+        public float Temperature { get; set; }
+
+        public void OnTemperatureChanged(float newTemperature)
+            =>  Console.WriteLine(newTemperature < Temperature
+                ? $"Heater: On. Changed:{Math.Abs(newTemperature - Temperature)}"
+                : $"Heater: Off. Changed:{Math.Abs(newTemperature - Temperature)}");
+    }
+
+    public class Thermostat
+    {
+        private float currentTemperature;
+
+        private const float Tolerance = 0.00001f;
+
+        public float CurrentTemperature
+        {
+            get => currentTemperature;
+            set
+            {
+                if (Math.Abs(value - currentTemperature) > Tolerance)
+                {
+                    currentTemperature = value;
+                }
+            }
+        }
+    }
+}
